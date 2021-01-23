7b586c0e165700425916cdf379fbc1955a726edf
Fergo <fergobirck@live.com>
2021-01-23 17:21:57 -0300

    Make things simples...

--- colorconvert.py
+++ colorconvert.py
@@ -2,9 +2,9 @@ import argparse
 import os
 import re
 
-parser = argparse.ArgumentParser(description='Convert colors of Townscaper blocks')
-parser.add_argument('saveFile', type=str, help='.scape file to process')
-parser.add_argument('mapFile', type=str, help='Text file containing each color to convert (one per line), in the format from:to')
+parser = argparse.ArgumentParser(description = 'Convert colors of Townscaper blocks')
+parser.add_argument('saveFile', type = str, help = '.scape file to process')
+parser.add_argument('mapFile', type = str, help = 'Text file containing each color to convert (one per line), in the format from:to')
 
 args = parser.parse_args()
 
@@ -19,20 +19,21 @@ if os.path.isfile(args.saveFile) == True and os.path.isfile(args.mapFile):
             split = line.split(":")
             mapping[split[0]] = split[1]
     
+    # open save file
     with open(args.saveFile, 'r') as saveFile:
-        saveFileLines = mapFile.read().splitlines()
+        saveFileLines = saveFile.read().splitlines()
 
-        corners = []
-        currentCorner, currentVoxel = 0, 0
-        for lineNo in len(saveFileLines):
-            if saveFileLines[lineNo].strip() == "</C>":
-                match = re.search(r'<count>(\d+)</count>', saveFileLines[lineNo - 1])
-                if match:
-                    corners.append(match.group(1))
-            
-            if saveFileLines[lineNo].strip() == "<V>":
-                originalColor = saveFileLines[lineNo + 1].replace("<t>", "").replace("</t>", "").strip()
+        # iterate on each line
+        for lineNo in range(len(saveFileLines)):
+            match = re.match(r'<t>(\d+)</t>', saveFileLines[lineNo].strip())
+
+            # if it matches a voxel type, replace its color (if exists in the mapping)
+            if match:
+                originalColor = match.group(1)
                 if originalColor in mapping:
-                    originalColor = re.sub(r'\d+', mapping[originalColor], saveFileLines[lineNo + 1]) 
+                    saveFileLines[lineNo] = re.sub(r'\d+', mapping[originalColor], saveFileLines[lineNo]) 
 
-                currentVoxel += 1
+        # dump the updated file
+        fileName = os.path.splitext(os.path.basename(args.saveFile))[0]
+        with open(args.saveFile.replace(fileName, fileName + "_colored"), 'w') as outFile:
+            outFile.writelines(s + '\n' for s in saveFileLines)
